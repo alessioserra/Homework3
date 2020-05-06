ef34999ea5a379206effcf4e393b8b85edbde993
zierp <zierp@LAPTOP-4TV42EQM>
2020-05-06 15:48:58 +0200

    DANN implemented

--- alexnetDANN.py
+++ alexnetDANN.py
@@ -1,5 +1,6 @@
 import torch
 import torch.nn as nn
+from utils import ReverseLayerF
 from .utils import load_state_dict_from_url
 
 
@@ -11,7 +12,7 @@ model_urls = {
 }
 
 
-class alexNetDNN(nn.Module):
+class alexnetDANN(nn.Module):
 
     def __init__(self, num_classes=1000):
         super(AlexNet, self).__init__()
@@ -41,15 +42,20 @@ class alexNetDNN(nn.Module):
             nn.Linear(4096, num_classes),
         )
 
-    def forward(self, x):
-        x = self.features(x)
-        x = self.avgpool(x)
-        x = torch.flatten(x, 1)
-        x = self.classifier(x)
+    def forward(self, x, alpha):
+        
+        if alpha is not None:
+           reversed_input = ReverseLayerF.apply(x, alpha)
+           x = self.discriminator(reversed_input)              
+        else:
+            x = self.features(x)
+            x = self.avgpool(x)
+            x = torch.flatten(x, 1)
+            x = self.classifier(x)
         return x
 
 
-def alexnetDNN(pretrained=False, progress=True, **kwargs):
+def alexnetDANN(pretrained=False, progress=True, **kwargs):
     r"""AlexNet model architecture from the
     `"One weird trick..." <https://arxiv.org/abs/1404.5997>`_ paper.
     Args:
